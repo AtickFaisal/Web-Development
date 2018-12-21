f08a3ceae461ba20c3d4cc9e50115de59ba8f0be
-1 <sa.akash0129@gmail.com>
2018-12-21 14:46:26 +0600

    contact pages fixed

--- ClientWebsiteDevs/Azizco/email-templates/subscribe-newsletter.php
+++ ClientWebsiteDevs/Azizco/email-templates/subscribe-newsletter.php
@@ -4,7 +4,7 @@
 	$from =$_POST["email"];
 	
 	// Email Receiver Address
-	$receiver="info@yourdomain.com";
+	$receiver="info@azizco.com";
 	$subject="Subscribe Newsletter form details";
 
 	$message = "
